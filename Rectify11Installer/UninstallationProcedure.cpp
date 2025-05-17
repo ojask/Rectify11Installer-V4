e07f77f4f8f056ea64a448edca21d7acc1ad4739
ojask <techno.mind69@gmail.com>
2025-05-17 21:34:39 +0530

    fix

--- Rectify11Installer/UninstallationProcedure.cpp
+++ Rectify11Installer/UninstallationProcedure.cpp
@@ -14,6 +14,12 @@ void RemoveWHMods() {
 		StringCchPrintf(exepath, MAX_PATH, L"%s\\System32\\cmd.exe", windir);
 		wchar_t args[] = L"/c reg delete HKLM\\SOFTWARE\\Windhawk\\Engine\\Mods\\logon-logoff-shutdown-sounds /f";
 		RunEXE(exepath, args);
+
+		InstallationLogger.WriteLine(L"Uninstalling uxtheme hook...");
+
+		StringCchPrintf(exepath, MAX_PATH, L"%s\\System32\\cmd.exe", windir);
+		wchar_t args2[] = L"/c reg delete HKLM\\SOFTWARE\\Windhawk\\Engine\\Mods\\uxtheme-hook /f";
+		RunEXE(exepath, args2);
 	}
 	if (InstallFlags[L"INSTALLICONS"] == true) {
 
